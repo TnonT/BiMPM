156b1fcb08b900640985bca4e553ec562b6af12a
ubuntu_ecnu_lab <1808132036@qq.com>
2019-04-01 22:19:49 +0800

    src

---
+++ src/aaaa.py
@@ -0,0 +1,33 @@
+# a = 'aaaaaaaaa'
+
+# b = a.encode(encoding = 'gbk')
+#
+# b = b.decode(encoding = 'utf-8')
+# print(b)
+
+# import time
+# import sys
+# for i in range(100):
+#     print('i' * 10)
+#     time.sleep(1)
+#
+# # sys.stdout.flush()
+
+# import numpy as np
+# a = [ i for i in range(20)]
+#
+# print(a)
+#
+# np.random.shuffle(a)
+#
+# print(a)
+#
+# b = sorted(a)
+#
+# print(b)
+# a = [1,2,3,4,5]
+# b = lambda a: (a[2], a[3])
+# print(b)
+a=1
+b=2
+print((a,b))
